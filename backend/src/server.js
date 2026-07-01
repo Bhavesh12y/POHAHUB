e750a3a4e0200a99be67e2905de7e6658ee5a225
Bhavesh12y <guptabhavi06@gmail.com>
2026-07-01 16:23:25 +0530

    leaderboard added at frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ import admin from "firebase-admin";
 
 
 const serviceAccount = JSON.parse(
-  readFileSync(new URL('./serviceAccountKey.json', import.meta.url))
+  readFileSync(new URL('../serviceAccountKey.json', import.meta.url))
 );
 
 admin.initializeApp({
